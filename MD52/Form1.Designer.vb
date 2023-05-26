df78f0f281bae3ca7e6b7b28f68f1a29183a6145
510208 <xux510208@gmail.com>
2023-05-26 21:24:43 +0800

    Release

--- MD52/Form1.Designer.vb
+++ MD52/Form1.Designer.vb
@@ -29,6 +29,14 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnCheckFile = New System.Windows.Forms.Button()
         Me.btnCopy = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.btnEnd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'OpenFileDialog1
@@ -37,9 +45,10 @@ Partial Class Form1
         '
         'btnOpenFile
         '
-        Me.btnOpenFile.Location = New System.Drawing.Point(592, 336)
+        Me.btnOpenFile.Location = New System.Drawing.Point(493, 222)
+        Me.btnOpenFile.Margin = New System.Windows.Forms.Padding(2)
         Me.btnOpenFile.Name = "btnOpenFile"
-        Me.btnOpenFile.Size = New System.Drawing.Size(144, 40)
+        Me.btnOpenFile.Size = New System.Drawing.Size(96, 27)
         Me.btnOpenFile.TabIndex = 0
         Me.btnOpenFile.Text = "開啟舊檔(&O)"
         Me.btnOpenFile.UseVisualStyleBackColor = True
@@ -47,60 +56,144 @@ Partial Class Form1
         'txtList
         '
         Me.txtList.BackColor = System.Drawing.Color.White
-        Me.txtList.Location = New System.Drawing.Point(12, 12)
+        Me.txtList.Location = New System.Drawing.Point(11, 11)
+        Me.txtList.Margin = New System.Windows.Forms.Padding(2)
         Me.txtList.Multiline = True
         Me.txtList.Name = "txtList"
         Me.txtList.ReadOnly = True
-        Me.txtList.Size = New System.Drawing.Size(724, 262)
+        Me.txtList.Size = New System.Drawing.Size(578, 172)
         Me.txtList.TabIndex = 1
         '
         'txtPath
         '
         Me.txtPath.BackColor = System.Drawing.Color.White
-        Me.txtPath.Location = New System.Drawing.Point(137, 280)
+        Me.txtPath.Location = New System.Drawing.Point(283, 187)
+        Me.txtPath.Margin = New System.Windows.Forms.Padding(2)
         Me.txtPath.Name = "txtPath"
         Me.txtPath.ReadOnly = True
-        Me.txtPath.Size = New System.Drawing.Size(599, 29)
+        Me.txtPath.Size = New System.Drawing.Size(306, 22)
         Me.txtPath.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 283)
+        Me.Label1.Location = New System.Drawing.Point(200, 192)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(119, 18)
+        Me.Label1.Size = New System.Drawing.Size(79, 12)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "檔案路徑(&P)："
         '
         'btnCheckFile
         '
-        Me.btnCheckFile.Location = New System.Drawing.Point(426, 336)
+        Me.btnCheckFile.Location = New System.Drawing.Point(382, 222)
+        Me.btnCheckFile.Margin = New System.Windows.Forms.Padding(2)
         Me.btnCheckFile.Name = "btnCheckFile"
-        Me.btnCheckFile.Size = New System.Drawing.Size(160, 40)
+        Me.btnCheckFile.Size = New System.Drawing.Size(107, 27)
         Me.btnCheckFile.TabIndex = 4
         Me.btnCheckFile.Text = "驗證完整性(&C)"
         Me.btnCheckFile.UseVisualStyleBackColor = True
         '
         'btnCopy
         '
-        Me.btnCopy.Location = New System.Drawing.Point(316, 336)
+        Me.btnCopy.Location = New System.Drawing.Point(309, 222)
+        Me.btnCopy.Margin = New System.Windows.Forms.Padding(2)
         Me.btnCopy.Name = "btnCopy"
-        Me.btnCopy.Size = New System.Drawing.Size(104, 40)
+        Me.btnCopy.Size = New System.Drawing.Size(69, 27)
         Me.btnCopy.TabIndex = 5
         Me.btnCopy.Text = "複製(&O)"
         Me.btnCopy.UseVisualStyleBackColor = True
         '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "TXT文字檔案(*.txt)|*.txt|所有檔案(*.*)|*.*"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(202, 222)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(102, 27)
+        Me.btnSave.TabIndex = 6
+        Me.btnSave.Text = "儲存成檔案(&S)"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = -16
+        Me.LineShape1.X2 = 600
+        Me.LineShape1.Y1 = 259
+        Me.LineShape1.Y2 = 259
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(600, 306)
+        Me.ShapeContainer1.TabIndex = 7
+        Me.ShapeContainer1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(511, 270)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(78, 27)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "關於(&A)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(239, 285)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(87, 12)
+        Me.LinkLabel1.TabIndex = 9
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "GitHub專案頁面"
+        '
+        'btnEnd
+        '
+        Me.btnEnd.Location = New System.Drawing.Point(429, 270)
+        Me.btnEnd.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnEnd.Name = "btnEnd"
+        Me.btnEnd.Size = New System.Drawing.Size(78, 27)
+        Me.btnEnd.TabIndex = 10
+        Me.btnEnd.Text = "離開(&E)"
+        Me.btnEnd.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(331, 270)
+        Me.btnUpdate.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(94, 27)
+        Me.btnUpdate.TabIndex = 11
+        Me.btnUpdate.Text = "檢查更新(&U)"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(748, 394)
+        Me.ClientSize = New System.Drawing.Size(600, 306)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnEnd)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.btnCopy)
         Me.Controls.Add(Me.btnCheckFile)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtPath)
         Me.Controls.Add(Me.txtList)
         Me.Controls.Add(Me.btnOpenFile)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "MD5驗證工具：新版"
         Me.ResumeLayout(False)
@@ -114,5 +207,13 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents btnCheckFile As System.Windows.Forms.Button
     Friend WithEvents btnCopy As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents btnEnd As System.Windows.Forms.Button
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
 
 End Class
--- MD52/Form1.vb
+++ MD52/Form1.vb
@@ -2,11 +2,43 @@
 Imports System.Text
 Imports System.IO
 Imports System.Windows.Forms.Clipboard
+Imports System.Net
+Imports System.Text.RegularExpressions
 
 Public Class Form1
     Public hashThings As New hashChecking
     Public Shared filePath
     Public Shared mdHash, shaHash, shaIHash, shaFITHash, nowTime, fileSize
+
+    Private Function GetLatestVersion(repository As String) As String
+        Dim apiUrl As String = "https://api.github.com/repos/" & repository & "/releases/latest"
+
+        Try
+            Dim client As New WebClient()
+            client.Headers.Add("User-Agent", "Mozilla/5.0") ' 設定使用者代理
+
+            ' 發送 HTTP GET 請求並獲取回應
+            Dim response As String = client.DownloadString(apiUrl)
+
+            ' 解析 JSON 回應，獲取最新版本號
+            Dim regex As New Regex("""tag_name"": ""(.*?)""")
+            Dim match As Match = regex.Match(response)
+            If match.Success Then
+                Return match.Groups(1).Value
+            Else
+                Return "無法獲取版本號"
+            End If
+        Catch ex As Exception
+            Return "檢查更新時發生錯誤: " & ex.Message
+        End Try
+    End Function
+
+    Sub updateChecker()
+        Dim repository As String = "510208/MD5Hashing-NEW" ' 請替換為實際的 GitHub 存儲庫
+
+        Dim latestVersion As String = GetLatestVersion(repository)
+        MessageBox.Show("最新版本號:" & latestVersion)
+    End Sub
 
     Private Sub btnOpenFile_Click(sender As Object, e As EventArgs) Handles btnOpenFile.Click
         OpenFileDialog1.ShowDialog()
@@ -30,15 +62,8 @@ Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.AllowDrop = True
-    End Sub
-
-    Private Sub Form1_DragDrop(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles MyBase.DragDrop
-        Dim DragFilePath As String = CType(e.Data.GetData("FileNameW"), Array).GetValue(0)
-        MsgBox(DragFilePath)
-    End Sub
-
-    Private Sub Form1_DragEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles MyBase.DragEnter
-        e.Effect = DragDropEffects.All
+        ' 初始化前一次的表單大小
+        Me.PreviousSize = Me.Size
     End Sub
 
     Private Sub btnCopy_Click(sender As Object, e As EventArgs) Handles btnCopy.Click
@@ -47,6 +72,95 @@ Public Class Form1
 
     Private Sub btnCheckFile_Click(sender As Object, e As EventArgs) Handles btnCheckFile.Click
         frmCheck.Show()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        SaveFileDialog1.ShowDialog()
+        ' 取得 TextBox 的文字內容
+        Dim textContent As String = txtList.Text
+
+        ' 將文字內容寫入到檔案
+        File.WriteAllText(SaveFileDialog1.FileName, textContent)
+
+        MessageBox.Show("檔案已儲存")
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Shell("Rundll32.exe url.dll, FileProtocolHandler https://github.com/510208/md5hashing-new", vbNormalFocus)
+    End Sub
+
+    Private Sub btnEnd_Click(sender As Object, e As EventArgs) Handles btnEnd.Click
+        End
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Call updateChecker()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        For Each control As Control In Me.Controls
+            ' 只處理可見的控制項
+            If control.Visible Then
+                ' 計算控制項的新位置和大小
+                Dim newLocation As New Point(control.Location.X * Me.Width / Me.PreviousSize.Width, control.Location.Y * Me.Height / Me.PreviousSize.Height)
+                Dim newSize As New Size(control.Width * Me.Width / Me.PreviousSize.Width, control.Height * Me.Height / Me.PreviousSize.Height)
+
+                ' 設定控制項的新位置和大小
+                control.Location = newLocation
+                control.Size = newSize
+            End If
+        Next
+
+        ' 更新前一次的表單大小
+        Me.PreviousSize = Me.Size
+
+        ' 將line長度設為總長
+        LineShape1.X2 = Me.Width
+
+    End Sub
+
+    Private Sub Form1_DragDrop(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles MyBase.DragDrop
+        Dim DragFilePath As String = CType(e.Data.GetData("FileNameW"), Array).GetValue(0)
+        MsgBox(DragFilePath)
+    End Sub
+
+    Private Property PreviousSize As Size
+
+    Private Sub gpbDropFile_DragEnter(sender As Object, e As DragEventArgs)
+        ' 確定拖曳的資料是檔案
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            ' 允許拖曳動作
+            e.Effect = DragDropEffects.Copy
+        Else
+            ' 禁止拖曳動作
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub gpbDropFile_DragDrop(sender As Object, e As DragEventArgs)
+        ' 取得拖曳進來的檔案路徑
+        Dim files As String() = DirectCast(e.Data.GetData(DataFormats.FileDrop), String())
+        If files.Length > 0 Then
+            ' 取得第一個檔案的路徑
+            filePath = files(0)
+            ' 在這裡可以進一步處理 filePath 變數
+            ' 例如顯示檔案路徑或進行其他相關操作
+            If filePath = "" Then
+                Exit Sub
+            End If
+            mdHash = hashChecking.md5(filePath)
+            shaHash = hashChecking.sha256(filePath)
+            shaIHash = hashChecking.sha1(filePath)
+            shaFITHash = hashChecking.sha512(filePath)
+            nowTime = Now.Hour & ":" & Now.Minute & ":" & Now.Second
+            fileSize = FileLen(filePath) / 1024
+            txtList.Text = "Time：" & nowTime & vbCrLf & "FileSize：" & Str(Math.Round(fileSize, 2)) & "MB" & vbCrLf & "MD5：" & mdHash & vbCrLf & "SHA256：" & shaHash & vbCrLf & "SHA1：" & shaIHash & vbCrLf & "SHA512：" & shaFITHash
+            txtPath.Text = filePath
+            frmCheck.md5Code.Text = mdHash
+            frmCheck.sha256Code.Text = shaHash
+            frmCheck.sha512Code.Text = shaFITHash
+            frmCheck.sha1Code.Text = shaIHash
+        End If
     End Sub
 End Class
 
